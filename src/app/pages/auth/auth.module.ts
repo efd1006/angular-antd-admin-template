1c071ae6b1e24f13f38237302c1ccd36b66cee98
Edmar Diaz <diaz.edmar@outlook.com>
2021-05-03 11:58:44 +0800

    refactor: added card ui on login page

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
@@ -20,6 +21,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     NzFormModule,
     NzButtonModule,
     NzInputModule,
+    NzCardModule,
+    NzLayoutModule
   ]
 })
 export class AuthModule { }
